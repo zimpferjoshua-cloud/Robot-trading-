996d48c95e220f2c0e12417c74b08131cad1f84b
zimpferjoshua-cloud <zimpferjoshua@gmail.com>
2026-06-28 10:58:54 +0200

    Create watchlist.py

---
+++ watchlist.py
@@ -0,0 +1,33 @@
+"""
+Liste des actions surveillées par le robot.
+Vous pouvez ajouter ou retirer des tickers librement.
+Format : symbole boursier tel que reconnu par Yahoo Finance.
+"""
+
+WATCHLIST = [
+    "AAPL",   # Apple
+    "MSFT",   # Microsoft
+    "GOOGL",  # Alphabet
+    "AMZN",   # Amazon
+    "NVDA",   # Nvidia
+    "META",   # Meta
+    "TSLA",   # Tesla
+    "JPM",    # JPMorgan Chase
+    "V",      # Visa
+    "JNJ",    # Johnson & Johnson
+    "WMT",    # Walmart
+    "PG",     # Procter & Gamble
+    "MA",     # Mastercard
+    "HD",     # Home Depot
+    "DIS",    # Disney
+    "BAC",    # Bank of America
+    "XOM",    # ExxonMobil
+    "PFE",    # Pfizer
+    "KO",     # Coca-Cola
+    "NFLX",   # Netflix
+    "ADBE",   # Adobe
+    "CRM",    # Salesforce
+    "INTC",   # Intel
+    "CSCO",   # Cisco
+    "PEP",    # PepsiCo
+]
